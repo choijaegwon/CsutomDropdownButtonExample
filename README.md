3eaae9952cc0dc7079cab4aca4ffa4bd5a7c7934
choijaegwon <chl9338@naver.com>
2024-01-08 23:11:09 +0900

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# CustomDropdownButton
+
+OverlayPortalController를 활용한 CustomDropdownButton  
+
+# Reference
+https://medium.com/codex/flutter-how-to-implement-a-nested-scrollable-bottom-sheet-c4830a2f8163   
---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Center(
+            child: CustomDropdownButton(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDropdownButton extends StatefulWidget {
+  const CustomDropdownButton({
+    super.key,
+  });
+
+  @override
+  State<StatefulWidget> createState() => CustomDropdownButtonState();
+}
+
+class CustomDropdownButtonState extends State<CustomDropdownButton> {
+  final OverlayPortalController _tooltipController = OverlayPortalController();
+
+  final _link = LayerLink();
+
+  /// width of the button after the widget rendered
+  double? _buttonWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _link,
+      child: OverlayPortal(
+        controller: _tooltipController,
+        overlayChildBuilder: (BuildContext context) {
+          return CompositedTransformFollower(
+            link: _link,
+            targetAnchor: Alignment.bottomLeft,
+            child: Align(
+              alignment: AlignmentDirectional.topStart,
+              child: MenuWidget(width: _buttonWidth),
+            ),
+          );
+        },
+        child: ButtonWidget(
+          onTap: onTap,
+          child: const Text('Button Text'),
+        ),
+      ),
+    );
+  }
+
+  void onTap() {
+    _buttonWidth = context.size?.width;
+    _tooltipController.toggle();
+  }
+}
+
+//
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    super.key,
+    this.height = 48,
+    this.width,
+    this.onTap,
+    this.child,
+  });
+
+  final double? height;
+  final double? width;
+
+  final VoidCallback? onTap;
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: Material(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: const BorderSide(color: Colors.black12),
+        ),
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(10),
+          child: Center(
+            child: child ?? const SizedBox(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({
+    super.key,
+    this.width,
+  });
+
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width ?? 200,
+      height: 300,
+      decoration: ShapeDecoration(
+        color: Colors.black26,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            width: 1.5,
+            color: Colors.black26,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        shadows: const [
+          BoxShadow(
+            color: Color(0x11000000),
+            blurRadius: 32,
+            offset: Offset(0, 20),
+            spreadRadius: -8,
+          ),
+        ],
+      ),
+    );
+  }
+}
